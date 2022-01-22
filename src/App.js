697d25b81aad786954e6665a7f9d0813a451ff42
Victor Saravia <victormsaraviat@gmail.com>
2022-01-22 11:33:36 -0500

    chore: add firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import firebase from './utils/Firebase';
+import "firebase/compat/auth";
 function App() {
+  firebase.auth().onAuthStateChanged(currentUser => {
+    console.log(currentUser?'Estamos logueados':'No estamos logueados');
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React + Electron</h1>
     </div>
   );
 }
